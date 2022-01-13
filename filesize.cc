ab6ca6d14b268866e95cb1191976fc1077ce573c
Kassady Herriott <kh557317@ohio.edu>
2022-01-13 18:30:53 -0500

    Attempt to fix

--- filesize.cc
+++ filesize.cc
@@ -1,118 +1,45 @@
-// //  Author: Kassady Herriott
-// //  Assignment: Homework 1 Operating Systems
-// //  Professor: Ostermann
-// //  Date: 13 January 2022
-
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-// #include <cctype>
-// using namespace std;
-
-// // output format to use IDENTICALLY:
-// //BSH:Saru> ./filesize input.3lines input.1line 
-// //program: ./filesize
-// // input.3lines: 3
-// // input.1line: 1
-
-// int main( int argc, char* argv[] )
-// {
-//     //stream for the files and variables for file name, line counts, bool to check for empty file case
-//     ifstream testFile;
-//     string fileName = "";
-//     int count = 0;
-//     int all_lines = 0;
-//     bool empty;
-//     string line = "";
-
-//     cout << "program: " << argv[0] << endl;
-
-//     for (int arg = 1; arg < argc; ++arg){
-//         empty = true;
-//         fileName = argv[arg];
-//         testFile.open(fileName);
-//         //if the file fails to open it outputs -1 
-//         if (!testFile.is_open()){
-//             cout << " " << fileName + ": " << -1 << endl; 
-//         } 
-//         else{
-//             //if file opens successfully do this
-//             while(testFile.good()){
-//                 getline(testFile, line);
-//                 //counts total number of lines
-//                 all_lines++;
-//                 //counts number of lines that arent new lines 
-//                 // if(line != "\n"){
-//                 //     count++;
-//                 //     empty = false;
-//                 // }
-//             }
-//             // if (empty){
-//                 //prints different counts based on empty lines or not
-//             cout << " " << fileName + ": " + to_string(all_lines) << endl;
-//             // }
-//             // else{
-//             // cout << " " << fileName + ": " + to_string(count) << endl;
-//             // }
-//             //resets variables 
-//             count = 0;
-//             all_lines = 0;
-//         }
-//         //closes files 
-//         testFile.close();
-//     }
-//     exit(0); // this means that the program executed correctly!
-// }
-
-
-
 //  Author: Kassady Herriott
 //  Assignment: Homework 1 Operating Systems
 //  Professor: Ostermann
 //  Date: 13 January 2022
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
-#include <cctype>
 using namespace std;
 
-// output format to use IDENTICALLY:
-//BSH:Saru> ./filesize input.3lines input.1line 
-//program: ./filesize
-// input.3lines: 3
-// input.1line: 1
-
-int main( int argc, char* argv[] )
-{
-    //stream for the files and variables for file name, line counts, bool to check for empty file case
+int main(int argc, char* argv[]) {
+    // stream for the files and variables for file name, line counts, bool to
+    // check for empty file case
     ifstream testFile;
     string fileName = "";
     int all_lines = 0;
     string line = "";
 
+    // grabs the program name from the first argument
     cout << "program: " << argv[0] << endl;
 
-    for (int arg = 1; arg < argc; ++arg){
+    // iterates through the arguments given as file names
+    for (int arg = 1; arg < argc; ++arg) {
+        // sets the file name to the argument given
         fileName = argv[arg];
         testFile.open(fileName);
-        //if the file fails to open it outputs -1 
-        if (!testFile.is_open()){
-            cout << " " << fileName + ": " << -1 << endl; 
-        } 
-        else{
-            //if file opens successfully do this
-            getline(testFile, line);
-            while(!testFile.eof()){
-                //counts total number of lines
-                all_lines++;
+        // if the file fails to open it outputs the file name and -1
+        if (!testFile.is_open()) {
+            cout << " " << fileName + ": " << -1 << endl;
+        } else {
+            // if file opens successfully do this
+            while (testFile.peek() != EOF) {
+                // adds to total number of lines
                 getline(testFile, line);
+                all_lines++;
             }
+            // outputs the filename and the number of lines associated with it
             cout << " " << fileName + ": " + to_string(all_lines) << endl;
             all_lines = 0;
         }
-        //closes files 
+        // closes files
         testFile.close();
     }
-    exit(0); // this means that the program executed correctly!
+    exit(0);  // this means that the program executed correctly!
 }
