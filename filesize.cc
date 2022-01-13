e4d0142da7608d2e2daf4f5001e2d631aeed5873
Kassady Herriott <kh557317@ohio.edu>
2022-01-13 11:47:28 -0500

    Testing fix for fifth test

--- filesize.cc
+++ filesize.cc
@@ -1,3 +1,8 @@
+//  Author: Kassady Herriott
+//  Assignment: Homework 1 Operating Systems
+//  Professor: Ostermann
+//  Date: 13 January 2022
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -42,13 +47,13 @@ int main( int argc, char* argv[] )
                     empty = false;
                 }
             }
-            if (empty){
-                //prints different counts based on empty lines or not
-                cout << " " << fileName + ": " + to_string(all_lines) << endl;
-            }
-            else{
+            // if (empty){
+            //     //prints different counts based on empty lines or not
+            //     cout << " " << fileName + ": " + to_string(all_lines) << endl;
+            // }
+            // else{
                 cout << " " << fileName + ": " + to_string(count) << endl;
-            }
+            // }
             //resets variables 
             count = 0;
             all_lines = 0;
