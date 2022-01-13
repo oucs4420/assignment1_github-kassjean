897bb3416c0f6f3b8e0f74046b51234b34ffb40f
Kassady Herriott <kh557317@ohio.edu>
2022-01-13 17:35:38 -0500

    Attempt to fix final test #6

--- filesize.cc
+++ filesize.cc
@@ -1,3 +1,71 @@
+// //  Author: Kassady Herriott
+// //  Assignment: Homework 1 Operating Systems
+// //  Professor: Ostermann
+// //  Date: 13 January 2022
+
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <cctype>
+// using namespace std;
+
+// // output format to use IDENTICALLY:
+// //BSH:Saru> ./filesize input.3lines input.1line 
+// //program: ./filesize
+// // input.3lines: 3
+// // input.1line: 1
+
+// int main( int argc, char* argv[] )
+// {
+//     //stream for the files and variables for file name, line counts, bool to check for empty file case
+//     ifstream testFile;
+//     string fileName = "";
+//     int count = 0;
+//     int all_lines = 0;
+//     bool empty;
+//     string line = "";
+
+//     cout << "program: " << argv[0] << endl;
+
+//     for (int arg = 1; arg < argc; ++arg){
+//         empty = true;
+//         fileName = argv[arg];
+//         testFile.open(fileName);
+//         //if the file fails to open it outputs -1 
+//         if (!testFile.is_open()){
+//             cout << " " << fileName + ": " << -1 << endl; 
+//         } 
+//         else{
+//             //if file opens successfully do this
+//             while(testFile.good()){
+//                 getline(testFile, line);
+//                 //counts total number of lines
+//                 all_lines++;
+//                 //counts number of lines that arent new lines 
+//                 // if(line != "\n"){
+//                 //     count++;
+//                 //     empty = false;
+//                 // }
+//             }
+//             // if (empty){
+//                 //prints different counts based on empty lines or not
+//             cout << " " << fileName + ": " + to_string(all_lines) << endl;
+//             // }
+//             // else{
+//             // cout << " " << fileName + ": " + to_string(count) << endl;
+//             // }
+//             //resets variables 
+//             count = 0;
+//             all_lines = 0;
+//         }
+//         //closes files 
+//         testFile.close();
+//     }
+//     exit(0); // this means that the program executed correctly!
+// }
+
+
+
 //  Author: Kassady Herriott
 //  Assignment: Homework 1 Operating Systems
 //  Professor: Ostermann
@@ -20,7 +88,6 @@ int main( int argc, char* argv[] )
     //stream for the files and variables for file name, line counts, bool to check for empty file case
     ifstream testFile;
     string fileName = "";
-    int count = 0;
     int all_lines = 0;
     bool empty;
     string line = "";
@@ -37,25 +104,13 @@ int main( int argc, char* argv[] )
         } 
         else{
             //if file opens successfully do this
-            while(testFile.good()){
-                getline(testFile, line);
+            getline(testFile, line);
+            while(!testFile.eof()){
                 //counts total number of lines
                 all_lines++;
-                //counts number of lines that arent new lines 
-                if(line != ""){
-                    count++;
-                    empty = false;
-                }
+                getline(testFile, line);
             }
-            // if (empty){
-                //prints different counts based on empty lines or not
-                cout << " " << fileName + ": " + to_string(all_lines) << endl;
-            // }
-            // else{
-            //     cout << " " << fileName + ": " + to_string(count) << endl;
-            // }
-            //resets variables 
-            count = 0;
+            cout << " " << fileName + ": " + to_string(all_lines) << endl;
             all_lines = 0;
         }
         //closes files 
